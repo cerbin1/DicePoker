fff1dddb17439eefeeea326ad302222040077c66
cerbin <bleble@d.com>
2016-10-14 20:04:40 +0200

    Fixed checking pairs.

--- src/Game.java
+++ src/Game.java
@@ -3,9 +3,13 @@ class Game {
     Player firstPlayer = new Player();
     Player secondPlayer = new Player();
 
+    ScoreChecker checkScore = new ScoreChecker();
+
     void run() {
         firstPlayer.randomDices();
         secondPlayer.randomDices();
         Console.displayDicesResult(firstPlayer);
+        checkScore.checkPairs(firstPlayer);
+        
     }
 }
--- src/ScoreChecker.java
+++ src/ScoreChecker.java
@@ -1,18 +1,28 @@
-public class ScoreChecker {
+class ScoreChecker {
     int pairs;
+    boolean[] pairChecker = {true, true, true, true, true};
 
     void checkPairs(Player player) {
-        if(((player.getDices(0) == 0) && player.getDices(1) == 0)) {
-            System.out.println("Mamy pare");
+        if(((player.getDices(0) == 0) && player.getDices(1) == 0) && (pairChecker[0]) && pairChecker[1]) {
+            System.out.println("Mamy pare indeksy 0 i 1");
+            pairChecker[0] = false;
+            pairChecker[1] = false;
         }
-        if(((player.getDices(1) == 0) && player.getDices(2) == 0)) {
-            System.out.println("Mamy pare");
+        if(((player.getDices(1) == 0) && player.getDices(2) == 0) && (pairChecker[1]) && pairChecker[2]) {
+            System.out.println("Mamy pare indeksy 1 i 2");
+            pairChecker[1] = false;
+            pairChecker[2] = false;
         }
-        if(((player.getDices(2) == 0) && player.getDices(3) == 0)) {
-            System.out.println("Mamy pare");
+        if(((player.getDices(2) == 0) && player.getDices(3) == 0) && (pairChecker[2]) && pairChecker[3]) {
+            System.out.println("Mamy pare indeksy 2 i 3");
+            pairChecker[2] = false;
+            pairChecker[3] = false;
         }
-        if(((player.getDices(3) == 0) && player.getDices(4) == 0)) {
-            System.out.println("Mamy pare");
+        if(((player.getDices(3) == 0) && player.getDices(4) == 0) && (pairChecker[3]) && pairChecker[4]) {
+            System.out.println("Mamy pare indeksy 3 i 4");
+            pairChecker[3] = false;
+            pairChecker[4] = false;
         }
+
     }
 }
