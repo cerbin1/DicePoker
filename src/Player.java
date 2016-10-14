5af1046dc57ff7349a2ce4e9b521f57ecf94f376
cerbin <bleble@d.com>
2016-10-14 18:37:20 +0200

    Changed randomDices().

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,14 @@
 class Player {
 
     private int[] playersDices = new int[6];
-    void run() {
+
+    void randomDices() {
         for (int i = 0; i < 6; i++) {
             playersDices[i] = (int) Math.floor( Math.random() * 6);
         }
+    }
+
+    void run() {
+
     }
 }
