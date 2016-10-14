8e7a8951912fdcf2bc555cbb489e44aab9e409dc
cerbin <bleble@d.com>
2016-10-14 20:34:49 +0200

    Changed the way of displaying array.

--- src/Console.java
+++ src/Console.java
@@ -1,7 +1,7 @@
 class Console {
     static void displayDicesResult(Player p, ScoreChecker s) {
         for (int i = 0; i < 5; i++) {
-            System.out.print(p.getDices(i) + ", ");
+            System.out.print(p.getDices()[i] + ", ");
         }
         System.out.println();
         for (int i = 0; i < 5; i++) {
