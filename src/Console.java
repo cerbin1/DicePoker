ee8107182158a1b68136605e6c098ae0a0823a9d
cerbin <bleble@d.com>
2016-10-15 01:12:38 +0200

    clean

--- src/Console.java
+++ src/Console.java
@@ -1,7 +1,7 @@
 import java.util.concurrent.TimeUnit;
 
 class Console {
-    static void displayDicesResults(Dices player, ScoreChecker s) {
+    static void displayDicesResults(Dices player) {
         System.out.println(player.getPlayersName() + " wylosowal: ");
         for (int i = 0; i < 5; i++) {
             if (i == 4) {
