2e7f1b70399a1783b99731f98c57b268b5458b8a
cerbin <bleble@d.com>
2016-10-14 23:41:31 +0200

    Extracted player.getDices() to variable diceNumbers.

--- src/Player.java
+++ src/Player.java
@@ -3,14 +3,9 @@ class Player {
     private int[] playersDices = new int[5];
 
     void randomDices() {
-/*        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             playersDices[i] = (int) Math.floor(Math.random() * 5);
-        }*/
-        playersDices[0] = 0;
-        playersDices[1] = 0;
-        playersDices[2] = 1;
-        playersDices[3] = 1;
-        playersDices[4] = 2;
+        }
     }
 
     int[] getDices() {
--- src/ScoreChecker.java
+++ src/ScoreChecker.java
@@ -4,7 +4,8 @@ class ScoreChecker {
 
     void groupDicesNumbers(Player player) {
         for (int i = 0; i < 5; i++) {
-            groupDiceNumbers[player.getDices()[i]]++;
+            int[] diceNumbers = player.getDices();
+            groupDiceNumbers[diceNumbers[i]]++;
         }
     }
 
