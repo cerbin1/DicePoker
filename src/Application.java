9a40c96a3df7354857c85ee4db3403b4f98377a8
cerbin <bleble@d.com>
2016-10-14 18:38:10 +0200

    Added Game class, moved run() into it.

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,7 @@
 public class Application {
 
     public static void main(String[] args) {
-        Player player = new Player();
-        player.run();
+        Game game = new Game();
+        game.run();
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -8,7 +8,5 @@ class Player {
         }
     }
 
-    void run() {
 
-    }
 }
