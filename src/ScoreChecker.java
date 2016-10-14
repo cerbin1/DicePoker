286827f7529835bbfef26f47db77315cd792896b
cerbin <bleble@d.com>
2016-10-14 22:05:50 +0200

    Added powerOfScore to check which player has better score.

--- src/ScoreChecker.java
+++ src/ScoreChecker.java
@@ -12,6 +12,10 @@ class ScoreChecker {
         return groupDiceNumbers;
     }
 
+    int getPowerOfScore() {
+        return powerOfScore;
+    }
+
     private boolean checkIfFull() {
         return ((groupDiceNumbers[0] == 3) && (groupDiceNumbers[1] == 2) || (groupDiceNumbers[0] == 3) && (groupDiceNumbers[2] == 2) || (groupDiceNumbers[0] == 3) && (groupDiceNumbers[3] == 2) ||
                 (groupDiceNumbers[0] == 3) && (groupDiceNumbers[4] == 2) || (groupDiceNumbers[0] == 3) && (groupDiceNumbers[5] == 2) || (groupDiceNumbers[1] == 3) && (groupDiceNumbers[0] == 2) ||
@@ -30,29 +34,35 @@ class ScoreChecker {
         for (int i = 0; i <= 5; i++) {
             if (groupDiceNumbers[i] == 5) {
                 System.out.println("Poker z " + (i + 1));
-                powerOfScore = 7;
+                powerOfScore = 8;
                 return;
             } else if (groupDiceNumbers[i] == 4) {
                 System.out.println("Kareta z " + (i + 1));
-                powerOfScore = 6;
+                powerOfScore = 7;
                 return;
             } else if (checkIfFull()) {
                 System.out.println("Full");
+                powerOfScore = 6;
                 return;
             } else if ((groupDiceNumbers[0] == 1) && (groupDiceNumbers[1] == 1) && (groupDiceNumbers[2] == 1) && (groupDiceNumbers[3] == 1) && (groupDiceNumbers[4] == 1)) {
                 System.out.println("Maly street");
+                powerOfScore = 5;
                 return;
             } else if ((groupDiceNumbers[1] == 1) && (groupDiceNumbers[2] == 1) && (groupDiceNumbers[3] == 1) && (groupDiceNumbers[4] == 1) && (groupDiceNumbers[5] == 1)) {
                 System.out.println("Duzy street");
+                powerOfScore = 4;
                 return;
             } else if (groupDiceNumbers[i] == 3) {
                 System.out.println("Trojka z " + (i + 1));
+                powerOfScore = 3;
                 return;
             } else if ((groupDiceNumbers[0] == 2) && (groupDiceNumbers[1] == 2)) { // TODO finish
                 System.out.println("Dwie pary, para 1 i para 2");
+                powerOfScore = 2;
                 return;
             } else if (groupDiceNumbers[i] == 2) { // TODO finish
                 System.out.println("Dwojka z " + (i + 1));
+                powerOfScore = 1;
                 return;
             }
         }
