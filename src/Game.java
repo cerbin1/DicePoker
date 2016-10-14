5982f5d67b3402cba336bd146cbbb9388371b220
cerbin <bleble@d.com>
2016-10-14 22:06:01 +0200

    Added second player to play.

--- src/Game.java
+++ src/Game.java
@@ -3,12 +3,15 @@ class Game {
     private Player firstPlayer = new Player();
     private Player secondPlayer = new Player();
     private ScoreChecker checkFirstPlayersScore = new ScoreChecker();
+    private ScoreChecker checkSecondPlayerScore = new ScoreChecker();
 
     void run() {
         firstPlayer.randomDices();
         secondPlayer.randomDices();
         checkFirstPlayersScore.groupDicesNumbers(firstPlayer);
         Console.displayDicesResults(firstPlayer, checkFirstPlayersScore);
+        Console.displayDicesResults(secondPlayer, checkSecondPlayerScore);
         checkFirstPlayersScore.checkScore();
+        checkSecondPlayerScore.checkScore();
     }
 }
