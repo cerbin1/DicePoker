f6daf5e56f655ce006cd83410ff6fd06e2a59172
cerbin <bleble@d.com>
2016-10-15 20:38:24 +0200

    Extracted randoming number into randomNumber()

--- src/Dices.java
+++ src/Dices.java
@@ -3,9 +3,12 @@ class Dices {
     private int[] playersDices = new int[5];
     private String playersName;
 
+    private int randomNumber() {
+        return (int) (Math.floor(Math.random() * 5) + 1);
+    }
     void randomDices() {
         for (int i = 0; i < 5; i++) {
-            playersDices[i] = (int) (Math.floor(Math.random() * 5) + 1); // TODO prawa strona równasię powinna isc do innej metody
+            playersDices[i] = randomNumber();
         }
     }
 
