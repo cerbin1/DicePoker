461d4465288157437e263f2749d6526404c71ba1
Daniel <wilkowski.kontakt@gmail.com>
2016-10-15 21:55:51 +0200

    Zmiana

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,9 @@
 public class Application {
-
+// komentarz u góry
     public static void main(String[] args) throws InterruptedException {
         Game game = new Game();
         game.run();
     }
 }
+
+// komentarz na dole
